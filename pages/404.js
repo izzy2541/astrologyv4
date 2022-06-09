67a470b79c4f5956b11dc32e280d997e87bec68e
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2022-06-09 11:34:42 +1000

    errorpage

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+
+function Error() {
+    return(
+        <div className="ErrorPage">
+            <div className="pageTitle">
+                ERROR 404
+            </div>
+            <div className = "smallText">
+                <p> Sorry! Page not found!</p>
+            </div>
+        </div>
+    
+    );
+  
+}
+
+export default Error;
