f5ca4b7ec0d7ce2859beea7d9ccf5e4f976499b0
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2022-06-09 10:51:14 +1000

    test

--- pages/privacy.js
+++ pages/privacy.js
@@ -9,7 +9,7 @@ function Privacy() {
             </div>
             <div className = "smallText">
                 <p> At Astrology.Com we adhere to the privacy principles outlined in The Privacy Act. To find out more,
-                    please click <a rel="noopener" target="_blank" href= "https://www.oaic.gov.au/privacy/australian-privacy-principles"><u>here.</u></a></p>
+                    please click <a  target="_blank" rel="noopener" href= "https://www.oaic.gov.au/privacy/australian-privacy-principles"><u>here.</u></a></p>
             </div>        
         </div>
     
