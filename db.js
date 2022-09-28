1fdb46156d7badbb7c106ae660272f7f6e6c65ba
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2022-09-28 14:16:54 +1000

    commit

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const signs = [
                 title: "Aries",
                 date: "March 21 - April 19",
                 element: "Fire",
-                description: "lorem ipsum",
+                description: "Aries loves to be number one, so it’s no surprise that these audacious rams are the first sign of the zodiac. Bold and ambitious, Aries dives headfirst into even the most challenging situations (which is appropriate, since the body part associated with Aries is the head). Like their fellow fire signs, Leo and Sagittarius, Aries is a passionate, motivated, and confident leader who builds community with their cheerful disposition and relentless determination. Uncomplicated and direct in their approach, they often get frustrated by exhaustive details and unnecessary nuances.",
                 image: "/images/aries.png"
               },
               {
@@ -12,7 +12,7 @@ export const signs = [
                 title: "Taurus",
                 date: "April 20 - May 20",
                 element: "Earth",
-                description: "lorem ipsum",      
+                description: "Taurus is an earth sign represented by the bull. Like their celestial spirit animal, Taureans enjoy relaxing in serene, bucolic environments, surrounded by soft sounds, soothing aromas, and succulent flavors. Taurus is ruled by Venus, the enchanting planet that governs love, beauty, and money. Taurus’s Venusian influence make this earth sign the most sensual of the zodiac: These cosmic oxen are enchanted by any physical manifestation of comfort and luxury. Pleasure is a necessity for epicurean Taureans and they feel most content when pampered. (Taurus governs the neck, so neck caresses are especially irresistible to these bulls.)",      
                 image: "/images/taurus.jpg"
               },
               {
@@ -20,7 +20,7 @@ export const signs = [
                 title: "Gemini",
                 date: "May 21 - June 20",
                 element: "Air",
-                description: "lorem ipsum",      
+                description: "Have you ever been so busy that you wished you could clone yourself just to get everything done? That’s the Gemini experience in a nutshell. Appropriately symbolized by the celestial twins, this air sign was interested in so many pursuits that it had to double itself. Because of Geminis' intrinsic duality, they're often falsely misrepresented as two-faced. In reality, however, Gemini rarely has a hidden agenda. Playful and intellectually curious, Gemini is constantly juggling a variety of passions, hobbies, careers, and friend groups. They are the social butterflies of the zodiac: These quick-witted twins can talk to anyone about anything. Find them buzzing between happy hours, dinner parties, and dance floors",      
                 image: "/images/gemini.png"
               },
               {
@@ -28,7 +28,7 @@ export const signs = [
                 title: "Cancer",
                 date: "June 21 - July 22",
                 element: "Water",
-                description: "lorem ipsum",      
+                description: "Cancer is a cardinal water sign. Represented by the crab, this oceanic crustacean seamlessly weaves between the sea and shore, representing Cancer’s ability to exist in both emotional and material realms. Cancers are highly intuitive and their psychic abilities manifest in tangible spaces: For instance, Cancers can effortlessly pick up the energies in a room. These crabs are highly sensitive to their environments, as well as extremely self-protective. Much like their celestial spirit animal, Cancers are shielded by hard, external shells. At first, these crabs may be perceived as cold or distant. With time, though, Cancers reveal their gentle nature, genuine compassion, and mystical capabilities. Just don't be surprised if it takes a while to get to know them.",      
                 image: "/images/cancerzodiac.jpg"
               },
               {
@@ -36,7 +36,7 @@ export const signs = [
                 title: "Leo",
                 date: "July 23 - August 22",
                 element: "Fire",
-                description: "lorem ipsum",      
+                description: "Roll out the red carpet, because Leo has arrived. Leo is represented by the lion, and these spirited fire signs are the kings and queens of the celestial jungle. They’re delighted to embrace their royal status: Vivacious, theatrical, and passionate, Leos love to bask in the spotlight and celebrate themselves. These lions are natural leaders and they enjoy cultivating friendships and romances that are artistically and creatively inspired. Playful Leos have no problem leaning into drama-fueled romances that are perfectly suited for the tabloids. (In fact, they may even prefer them.) After all, every Leo perceives him or herself as a celebrity. These astrological divas never get tired of lavish dinners, exclusive parties, or decadent designer wear.",      
                 image: "/images/leozodiac.jpg"
               },
               {
@@ -44,7 +44,7 @@ export const signs = [
                 title: "Virgo",
                 date: "August 23 - September 22",
                 element: "Earth",
-                description: "lorem ipsum",      
+                description: "Virgo is an earth sign historically represented by the goddess of wheat and agriculture, an association that speaks to Virgo’s deep-rooted presence in the material world. Virgos are logical, practical, and systematic in their approach to life. This earth sign is a perfectionist at heart and isn’t afraid to improve skills through diligent and consistent practice. Virgo rules the digestive system, which makes these earth signs especially attuned to the ingredients that make up a whole — in food and in everything else. They're hyper-aware of every detail.",      
                 image: "/images/virgozodiac.jpg"
               },
           
@@ -53,7 +53,7 @@ export const signs = [
                   title: "Libra",
                   date: "September 23 - October 22",
                   element: "Air",
-                  description: "lorem ipsum",      
+                  description: "Libra is an air sign represented by the scales (interestingly, the only inanimate object of the zodiac), an association that reflects Libra's fixation on balance and harmony. Libra is obsessed with symmetry and strives to create equilibrium in all areas of life. These air signs are the aesthetes of the zodiac: Ruled by Venus, the planet that governs love, beauty, and money, Libras adore high art, intellectualism, and connoisseurship. Suave Libras need to surround themselves with stunning objects and create environments that reflect their exquisite tastes. Accordingly, these air signs make excellent designers, decorators, art critics, and stylists.",      
                   image: "/images/libra.jpg"
                 },
                 {
@@ -61,7 +61,7 @@ export const signs = [
                   title: "Scorpio",
                   date: "October 23 - November 21",
                   element: "Water",
-                  description: "lorem ipsum",      
+                  description: "Scorpio is one of the most misunderstood signs of the zodiac. Because of its incredible passion and power, Scorpio is often mistaken for a fire sign. In fact, Scorpio is a water sign that derives its strength from the psychic, emotional realm. Like fellow water signs, Cancer and Pisces, Scorpio is extremely clairvoyant and intuitive. What makes this water sign unique is its distinctive venomous sting. Like their celestial spirit animal, the scorpion, Scorpios lie in wait and strike when least expected. Life is a game of chess for these calculating water signs, who are constantly plotting several steps ahead in order to orchestrate an eventual checkmate. This doesn't mean their intentions are necessarily nefarious. Scorpios simply know what they want and arent afraid to work hard and play the long game to get it.",      
                   image: "/images/scorpiozodiac.jpg"
                 },
                 {
@@ -69,7 +69,7 @@ export const signs = [
                   title: "Sagittarius",
                   date: "November 22 - December 21",
                   element: "Fire",
-                  description: "lorem ipsum",      
+                  description: "The final fire sign of the zodiac, Sagittarius traits are unlike any other sign of the zodiac; they're totally unique to this brazen spirit. As a professional astrologer with almost 10 years of experience, I can honestly say that there is no zodiac sign like Sagittarius. What makes Sagittarius so unique is its dynamic blend of passion, curiosity, intensity, and adaptability. Represented by the archer (a half-man, half-horse centaur), Sagittarius isn't afraid to use its bow and arrow to explore expansive terrain, seeking answers in places and spaces others wouldn’t dare venture. Whether they're white water rafting down a river in some undisclosed location or taking a pilgrimage to a sacred site to uncover secrets about an ancient civilization, Sagittarius’ quest for knowledge knows no bounds.",      
                   image: "/images/sagittariuszodiac.jpg"
                 },
                 {
@@ -77,7 +77,7 @@ export const signs = [
                   title: "Capricorn",
                   date: "December 22 - January 19",
                   element: "Earth",
-                  description: "lorem ipsum",      
+                  description: "The last earth sign of the zodiac, Capricorns and their unique spirits are powerful, to say the least. I've worked with thousands of clients over my nearly-10 year-long career as a professional astrologer, and I can confirm that there's something that makes Capricorn extremely special. Perhaps it's their fearless ambition? Their limitless resilience? Their ability to keep pushing forward, even in the face of challenging adversity and painful strife? Or, maybe it's their secret wild side that's very rarely depicted in standard personality assessments, but that rumbles within the soul of every Capricorn: Not everyone gets to see Capricorn ordering rounds of tequila, dancing on the table, and staying out until the crack of dawn — but that rebellious, untamed spirit is a Capricorn asset. I mean, how else do you expect this zodiac sign to let loose and have fun?",      
                   image: "/images/capricornzodiac.jpg"
                 },
                 {
@@ -85,7 +85,7 @@ export const signs = [
                   title: "Aquarius",
                   date: "January 20 - Febuary 18",
                   element: "Water",
-                  description: "lorem ipsum",      
+                  description: "As you can imagine, I encounter a lot of misconceptions as a professional astrologer — not just regarding my career as a stargazer, but also relating to the technical aspects of astrology. One of the most ubiquitous, albeit innocuous, errors is that Aquarius is a water sign. I could certainly understand the confusion here — especially with "aqua" as the prefix — but once you get to know this zodiac sign, it becomes strikingly clear that Aquarius is an air sign. As the eleventh and penultimate sign of the zodiac, Aquarius is actually the final air sign, which means it deals with air-related concepts (we'll get more into that momentarily) from a macro-perspective. Of all the zodiac signs, Aquarius is undoubtedly the most innovative, progressive, rebellious, and humanitarian. And while Aquarius can often be found planning a revolution or proudly flaunting their funky fashion sensibility, they also have an often overlooked sensitive side that requires appreciation, support, and love.",      
                   image: "/images/aquariuszodiac.jpg"
                 },
                 {
@@ -93,7 +93,7 @@ export const signs = [
                   title: "Pisces",
                   date: "February 19 - March 20",
                   element: "Waterr",
-                  description: "lorem ipsum",      
+                  description: "Pisces, a water sign, is the last constellation of the zodiac. It's symbolized by two fish swimming in opposite directions, representing the constant division of Pisces's attention between fantasy and reality. As the final sign, Pisces has absorbed every lesson — the joys and the pains, the hopes and the fears — learned by all of the other signs. This makes these fish the most psychic, empathetic, and compassionate creatures of the astrological wheel. With such immense sensitivity, Pisces can easily become swallowed by emotions and must remember to stay grounded in the material realm (appropriately, Pisces rules the feet).",      
                   image: "/images/pisces.jpg"
                 }
   
